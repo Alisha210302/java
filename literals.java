12ff2ca017e46d5914bb8b7135f42180ed0d9d0b
Alisha210302 <alishabandal95@gmail.com>
2024-09-24 21:21:35 +0530

    Add files via upload

---
+++ literals.java
@@ -0,0 +1,16 @@
+public class literals {
+    public static void main(String[] args) {
+        byte age = 34; // right side value is called as literal
+        int age2 = 55;
+        short age3 = 34;
+        long ageDino = 2345656745453L;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 4.66d; // D or d
+        boolean b = true;
+        String a = "Alisha";
+        System.out.println(age);
+        System.out.println(ch);
+        System.out.println(a);
+    }
+}
