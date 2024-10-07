23e8b26f7e929ee5edf64681000bc6e9a89faddc
Alisha210302 <alishabandal95@gmail.com>
2024-10-07 22:23:09 +0530

    Add files via upload

---
+++ Calender.java
@@ -0,0 +1,13 @@
+
+public class Calender {
+
+    public static void main(String[] args)
+    {
+        NewDate d = new NewDate();
+        d.setDay(11);
+        d.setMonth("jan");
+        d.setYear(2024);
+
+        System.out.println(d);
+    }
+}
